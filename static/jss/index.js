3424325a33345ecb97356abb2f9554a0e0f4ebff
Ciofaaa <167424659+Ciofaaa@users.noreply.github.com>
2025-05-29 11:11:24 +0200

    finito

---
+++ static/jss/index.js
@@ -0,0 +1,33 @@
+ url = "https://randomuser.me/api/";
+let glbPerson;
+
+let n = document.querySelector(".name");
+let s = document.queletrySelector(".surname");
+let e = document.querySelector(".email");
+let p = document.querySelector(".phone");
+let d = document.querySelector(".dob");
+let l = document.querySelector(".location");
+let c = document.querySelector(".country");
+let img = document.querySelector(".picture")
+
+fetch(url).then(
+    function (resp) {
+        console.log(resp)
+
+        return resp.json()
+    }
+).then(
+    function (glbPerson) {
+        img.setAttribute("src", glbPerson.results[0].picture.large);
+        n.innerHTML = glbPerson.results[0].name.first + " " + glbPerson.results[0].name.last;
+        e.innerHTML = glbPerson.results[0].email;
+        p.innerHTML = "+ " + glbPerson.results[0].phone;
+        d.innerHTML = glbPerson.results[0].dob.age + " years";
+        l.innerHTML = glbPerson.results[0].location.street.name + " " + glbPerson.results[0].location.street.number;
+        c.innerHTML = glbPerson.results[0].location.state + ", " + glbPerson.results[0].location.country;
+    }
+).catch(
+    function (err) {
+        console.log(err)
+    }
+)
